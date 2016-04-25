9d98775557c61a0f91b52f38604c714783a4a4c2
Angel Jimenez <ajl890889702@csu.fullerton.edu>
2016-04-25 13:06:53 -0700

    added main.cpp

--- Memory_management/main.cpp
+++ Memory_management/main.cpp
@@ -7,9 +7,106 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <queue>
+#include <fstream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+struct Process {
+    int pid;
+    int time_start;
+    int time_end;
+    int mem_block_count;
+    vector<int> mem_blocks;
+};
+
+void readFile(queue<Process> & process_queue);
+
+int main()
+{
+    string file = "in1.txt";
+    int memory_size = 0;
+    int page_size= 0;
+    int choice;
+    queue<Process> process_queue;
+    
+    cout << "Enter the memory size(Kbytes): ";
+    cin >> memory_size;
+    cout << "Enter the page size (1:100, 2:200, 3:400): ";
+    cin >> choice;
+    
+    switch(toupper(choice)){
+        case 1:
+            page_size = 100;
+            break;
+        case 2:
+            page_size = 200;
+            break;
+        case 3:
+            page_size = 400;
+            break;
+    }
+    
+    //DO SOMETHING...
+    readFile(process_queue);
+    //findTimes();
+    //printOutput();
+    
     return 0;
 }
+
+void readFile(queue<Process> & process_queue){
+    
+    Process process;
+    vector<int> mem_blocks;
+    int process_count, mem_block;
+    
+    ifstream input_file;
+    input_file.open("in1.txt");
+    
+    input_file >> process_count;
+    
+    for(int i = 0; i < process_count; i++){
+        input_file >> process.pid;
+        input_file >> process.time_start;
+        input_file >> process.time_end;
+        input_file >> process.mem_block_count;
+       
+        for(int j = 0; j < process.mem_block_count; j++){
+            input_file >> mem_block;
+            mem_blocks.push_back(mem_block);
+        }
+        
+        process.mem_blocks = mem_blocks;
+        process_queue.push(process);
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
